d1ea539c8b33b1eab79fd2f8f63670141e4dc196
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-04-26 21:49:17 +0300

    Занятие VII.2 Основы RSpec

--- rspec/example_spec.rb
+++
@@ -1,8 +0,0 @@
-RSpec.describe 'Ruby успешно выполняет' do
-  it 'сложение двух чисел' do
-    expect(2 + 2).to be 4
-  end
-  it 'генерацию массива из диапазона' do
-    expect([*1..10]).to include 5
-  end
-end
