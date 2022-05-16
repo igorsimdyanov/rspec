ad807057cd0d96d39d06e515642cee015cc8c9aa
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-05-16 13:29:11 +0300

    Тестирование класса Солнечной системы

---
+++ rspec/spec/solar_system_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe SolarSystem do
+  it 'содержит методы-планеты' do
+    %i[mercury venus earth mars jupiter saturn uranus neptune].each_with_index do |planet, i|
+      expect(described_class.send(planet)).to eq i + 1
+    end
+  end
+
+  it 'обращение к несуществующему методу' do
+    expect { SolarSystem.wrong }.to raise_error('Wrong planet')
+  end
+end
--- rspec/spec/spec_helper.rb
+++ rspec/spec/spec_helper.rb
@@ -2,9 +2,8 @@ require 'set'
 require 'rspec/collection_matchers'
 require_relative 'support/array_builder'
 require_relative 'support/enumerable_shared_examples'
-require_relative '../lib/user'
 
-Dir['lib/services/*.rb'].each do |file|
+Dir['lib/**/*.rb'].each do |file|
   require_relative "../#{file}"
 end
 
